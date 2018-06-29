3b4033b155011a63bdd0ef0c1298c286fba9013a
juko <funscript@outlook.fr>
2018-06-29 21:50:32 +0000

    little format and input bug fix

--- Lotus.py
+++ Lotus.py
@@ -22,34 +22,43 @@ class Engine(object):
 			
 class Prolouge(Scene):
 	def enter(self,MainCharacter):
-		print "Lotus in The Underworld"
-		print "developed by Lestari Ningratna Sari"
-		print "2018"
-		print "--------"
-		print "You just woke up in a big and luxurious bedroom. You can't remember anything."
-		print "You try to recall, and it's getting weird because you don't even remember your name."
-		print "From large windows across the room in front of you, the bright morning light striking into the room."
-		print "You see a wooden table near the windows."
-		print "There's a letter on the table. Type \"read\" to read the letter."
+		print """\033[1;31m
+                     Lotus in The Underworld
+		     developed by Lestari Ningratna Sari
+	             2018 
+                     \x1b[0m
+                      """
+		print """
+                                                        --------
+		     You just woke up in a big and luxurious bedroom. You can't remember anything.
+		     You try to recall, and it's getting weird because you don't even remember your name.
+		     From large windows across the room in front of you, the bright morning light striking into the room.
+	             You see a wooden table near the windows."
+	             There's a letter on the table. Type \"read\" to read the letter.
+                     """
 		
-		action = raw_input("> ")
+		action = raw_input("> ").lower()
 		if action == "read":
-			print "\n+==================================================================================================================+"
-			print "|\t \t \t \t \t   WELCOME TO THE UNDERWORLD                                               |"
-			print "|                                                                                                                  |"
-			print "+==================================================================================================================+"
-			print "| This place is a dangerous place that can mislead anyone in it. If you are stranded here, you need to escape.     |"
-			print "| To be able to escape from the underworld, you need to finish your mission.                                       |"
-			print "| And to be able to do it, you need to know what your mission is.                                                  |"
-			print "| Unfortunately, you will never find the answer here.                                                              |"
-			print "| You need to regain your memory to find out what your mission is.                                                 |"
-			print "| But don't worry, you will get a beautiful lotus in your hand to guide you and find the answer.                   |"
-			print "|                                                                                                                  |"
-			print "|\t \t \t \t \t \t    *****                                                          |"
-			print "+==================================================================================================================+"
-			print "                                                    ....."
-			print "                                                     ..."
-			print "                                                      ."
+			print """
+                                  +==================================================================================================================+"
+			         "|                                          WELCOME TO THE UNDERWORLD                                               |"
+		        	 "|                                                                                                                  |"
+		        	 "+==================================================================================================================+"
+		        	 "| This place is a dangerous place that can mislead anyone in it. If you are stranded here, you need to escape.     |"
+		        	 "| To be able to escape from the underworld, you need to finish your mission.                                       |"
+		        	 "| And to be able to do it, you need to know what your mission is.                                                  |"
+			         "| Unfortunately, you will never find the answer here.                                                              |"
+		        	 "| You need to regain your memory to find out what your mission is.                                                 |"
+		        	 "| But don't worry, you will get a beautiful lotus in your hand to guide you and find the answer.                   |"
+		        	 "|                                                                                                                  |"
+		        	 "|                                                   *****                                                          |"
+		        	 "+==================================================================================================================+"
+		        	 "                                                    ....."
+		        	 "                                                     ..."
+			 
+                                                                                         .
+                               """
+
 			return 'lotus_mansion'
 		else:
 			print "DOES NOT COMPUTE!"
@@ -57,74 +66,87 @@ class Prolouge(Scene):
 			
 class LotusMansion(Scene):
 	def enter(self,MainCharacter):
-		print "A beautiful lotus magically appears in your hand out of thin air. A blooming red lotus."
-		print "Very unusual because the color is bright red like the color of blood."
-		print "You look at the flower and wondering how it suddenly appears, a sudden loud voice startled you."
-		print "Someone's knocking the front door. You step out of the bedroom, walking through the hallway to reach the front door."
-		print "The house is so big and spacious it's more similar to mansion than a normal house."
-		print "You open the door and you see a little boy with his white pyrenean mountain dog."
-		print "\"Sir, my dog has been following the smell of lotus flower in your house.\""
-		print "\"My dog told me that you can help me find my home.\""
-		print "\"I've been walking for 3 days with my dog, and I couldn't find my way to home.\""
-		print "\"Would you mind to help me? Please...\", said the little boy"
-		print "You see the lotus in your hand is shining so bright, and suddenly one of its petals fall off."
-		print "This little boy could be a clue for you to escape the underworld. Do you want to help him? (y/n)"
+		print """
+                                A beautiful lotus magically appears in your hand out of thin air. A blooming red lotus.
+		                Very unusual because the color is bright red like the color of blood.
+		                You look at the flower and wondering how it suddenly appears, a sudden loud voice startled you.
+		                Someone's knocking the front door. You step out of the bedroom, walking through the hallway to reach the front door.
+		                The house is so big and spacious it's more similar to mansion than a normal house.
+		                You open the door and you see a little boy with his white pyrenean mountain dog.
+		                \"Sir, my dog has been following the smell of lotus flower in your house.\"
+		                \"My dog told me that you can help me find my home.\""
+		                \"I've been walking for 3 days with my dog, and I couldn't find my way to home.\"
+		                \"Would you mind to help me? Please...\", said the little boy"
+		                You see the lotus in your hand is shining so bright, and suddenly one of its petals fall off.
+		                This little boy could be a clue for you to escape the underworld. Do you want to help him? (y/n)
+                    """
 		
-		action = raw_input("> ")
+                action = raw_input("> ").lower()
+                
 		if action == "y":
-			print "---------------------------------------------------------"
-			print "You decided to help him. Try to ask something to him!"
-			print "1. \"What\'s your name?\""
-			print "2. \"Where do you live?\""
-			print "3. \"Your dog can talk?\""
-			print "4. \"Do you know anything about my weird flower?\""
-			print "5. \"alright. Let's find your home\""
+			print """
+                                ---------------------------------------------------------
+			        You decided to help him. Try to ask something to him!"
+			        1. \"What\'s your name?\"
+			        2. \"Where do you live?\"
+			        3. \"Your dog can talk?\"
+			        4. \"Do you know anything about my weird flower?\"
+			        5. \"alright. Let's find your home\"
+                         """
 			return 'dialouge'
 		
 		elif action == "n":
-			print "Like a world class boxer you dodge, weave, slip and slide right"
-			print "as the Gothon's blaster cranks a laser past your head."
-			print "In the middle of your artful dodge your foot slips and you"
-			print "bang your head on the metal wall and pass out."
-			print "You wake up shortly after only to die as the Gothon stomps on"
-			print "your head and eats you."
-			
+			print """
+                                Like a world class boxer you dodge, weave, slip and slide right"
+			        as the Gothon's blaster cranks a laser past your head."
+			        In the middle of your artful dodge your foot slips and you"
+			        bang your head on the metal wall and pass out."
+			        You wake up shortly after only to die as the Gothon stomps on"
+			        your head and eats you."
+			"""
 		else:
 			print "DOES NOT COMPUTE!"
 			return 'lotus_mansion'
 
 class Dialouge(Scene):
 	def enter(self,MainCharacter):
-		action = raw_input("> ")
+		action = raw_input("> ").lower() 
+
 		if action == "1":
-			print "You asked: 1. \"What's your name?\""
-			print "\"my name is Liam... Liam Dayton. and my dog's name is Snowy\", said the kid."
-			print "\"May I know your name, sir?\""
-			print "You don't know what to say, because you don't even remember yours"
-			print "\"Uh.... I don't remember my name\""
-			print "\"That's weird. Okay.... can I call you Mr. Lotus?\", asked the kid"
-			print "\"It sounds funny. Well, it's okay... you can call me Mr. Lotus. Nice to meet you Liam!\""
+			print """
+                               You asked: 1. \"What's your name?\"
+			       \"my name is Liam... Liam Dayton. and my dog's name is Snowy\", said the kid.
+			       \"May I know your name, sir?\"
+			       You don't know what to say, because you don't even remember yours
+			       \"Uh.... I don't remember my name\"
+			       \"That's weird. Okay.... can I call you Mr. Lotus?\", asked the kid
+			       \"It sounds funny. Well, it's okay... you can call me Mr. Lotus. Nice to meet you Liam!\"
+                         """
 			return 'dialouge'
 		
 		elif action == "2":
-			print "You asked: 2. \"Where do you live?\""
-			print "\"I don't remember...\""
-			print "\"That's why I asked for your help\""
-			print "\"Snowy knows where is it. But he's blind and he can't smell very well\""
-			print "\"The only thing he can smell is your flower\""
+			print """
+                               You asked: 2. \"Where do you live?\"
+			       \"I don't remember...\""
+			       \"That's why I asked for your help\"
+			       \"Snowy knows where is it. But he's blind and he can't smell very well\"
+			       \"The only thing he can smell is your flower\"
+                         """
 			return 'dialouge'
 		
 		elif action == "3":
-			print "You asked: 3. \"Your dog can talk?\""
-			print "\"You can't hear it? Look he just talked to me!\", said Liam"
-			print "\"No, I can't hear it.\" you said"
-			print "\"You can't hear it? but he just talked\""
-			print "\"woof!\", the dog barked and wagging his tail"
-			print "You think this kid's imagination is too wild."
-			print "But after all of the weird things that just happened to you,"
-			print "it might be real. This kid might be able to talk to his dog"
-			print "\"what did Snowy say to you?\""
-			print "\"He said you smell like a good person and he trust you\""
+			print """
+                              You asked: 3. \"Your dog can talk?\"
+			      \"You can't hear it? Look he just talked to me!\", said Liam
+			      \"No, I can't hear it.\" you said
+			      \"You can't hear it? but he just talked\"
+			      \"woof!\", the dog barked and wagging his tail
+			      You think this kid's imagination is too wild.
+			      But after all of the weird things that just happened to you,
+			      it might be real. This kid might be able to talk to his dog
+			      \"what did Snowy say to you?\""
+			      \"He said you smell like a good person and he trust you\"
+                                """
 			return 'dialouge'
 		
 		else:
